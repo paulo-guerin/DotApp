48960f3bc656dda902c002d1eb7ea7977f652a61
paulo-guerin <paularguerin@gmail.com>
2023-02-16 11:09:20 +0100

    init

---
+++ DotApp/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace DotApp
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
